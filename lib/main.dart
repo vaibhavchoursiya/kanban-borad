3733f53276a4586cb0d3e5ecea837d9310289ede
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2024-12-18 12:42:30 +0530

    set min size for windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kanban_board/providers/home_provider.dart';
 import 'package:kanban_board/providers/task_panel_provider.dart';
@@ -5,11 +8,19 @@ import 'package:kanban_board/routes.dart';
 import 'package:kanban_board/services/db_service.dart';
 import 'package:provider/provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_size/window_size.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   sqfliteFfiInit();
 
   await DbService.setupDB();
+
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    setWindowMinSize(const Size(990, 600));
+    setWindowMaxSize(Size.infinite);
+  }
 
   runApp(const MyApp());
 }
--- lib/widgets/card_grid.dart
+++ lib/widgets/card_grid.dart
@@ -11,20 +11,25 @@ class CardGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: SizedBox(
-        width: 700.0,
-        child: GridView.builder(
-          itemCount: context.watch<HomeProvider>().collectionNames.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisExtent: 210.0,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 18.0, bottom: 18.0, right: 18.0),
+        child: SizedBox(
+          width: 700.0,
+          child: GridView.builder(
+            // shrinkWrap: true,
+            // physics: NeverScrollableScrollPhysics(),
+            itemCount: context.watch<HomeProvider>().collectionNames.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisExtent: 210.0,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+            ),
+            itemBuilder: (context, index) {
+              final obj = context.read<HomeProvider>().collectionNames[index];
+              return CollectionCard(collectionName: obj["cn"]);
+            },
           ),
-          itemBuilder: (context, index) {
-            final obj = context.read<HomeProvider>().collectionNames[index];
-            return CollectionCard(collectionName: obj["cn"]);
-          },
         ),
       ),
     );
